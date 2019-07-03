ef040857e538264834dbe6ef9074151c39a93b99
oscarii <52502738+oscarii@users.noreply.github.com>
2019-07-03 12:52:45 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Producto.h"
+#include "Perecederos.h"
+#include <stdio.h>
+using namespace std;
+
+
+int main()
+{
+   /*Producto p("jabon",20);*/
+   Perecederos *p=new Perecederos("jabon",10,3);
+
+}
